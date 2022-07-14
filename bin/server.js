28bf08bf7ea5bc372899ca90f1eaa8fadcda3b81
Anatoliy <78277944+AnatoliyDanets@users.noreply.github.com>
2022-07-14 20:15:10 +0300

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,8 @@
 const app = require("../app");
-const mongoose=require('mongoose')
-const dotenv=require('dotenv')
-dotenv.config()
-const {DB_HOST}=process.env
-const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
@@ -12,7 +11,7 @@ mongoose
       console.log(`Database connection successful`);
     });
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(`Server not run. Error: ${err.message}`);
     process.exit(1);
   });
